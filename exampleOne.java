a615749a374f2cf5520afd7768c0f32533795856
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-09-12 14:34:40 +0300

    CSC211_ExampleOne

---
+++ exampleOne.java
@@ -0,0 +1,31 @@
+//Outputs
+
+public class exampleOne {
+    public static void main(String[] args) {
+        // Arithematic Operators
+        int a = 10;
+        int b = 5;
+        // Addition
+        int sum = a + b;
+
+        // Subtraction
+        int difference = a - b;
+
+        // Multiplication
+        int product = a * b;
+
+        // Division
+        int quotient = a / b;
+
+        // Modulus
+        int remainder = a % b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+
+        System.out.println("Sum: " + sum);
+    }
+}
