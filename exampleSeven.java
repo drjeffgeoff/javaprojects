dac7e5fbfdcbacab8baccbf6905653b941c14ebe
drjeffgeoff <jeff.geoff.phd@gmail.com>
2024-09-13 11:20:45 +0300

    Added code for exampleSeven

---
+++ exampleSeven.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class exampleSeven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number (1-7): ");
+        int day = scanner.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day number.");
+        }
+        scanner.close();
+    }
+}
