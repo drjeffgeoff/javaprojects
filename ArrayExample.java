2b9dce1ff4e89b2e6378bfe231cfb7b66f686c13
Jeff Geoff <jeff.geoff.phd@gmail.com>
2024-09-24 10:18:02 +0300

    Declaring, Initializing, and Accessing Arrays

---
+++ ArrayExample.java
@@ -0,0 +1,11 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Declaring and initializing an array of integers with 5 elements
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Accessing and printing each element in the array using a loop
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
